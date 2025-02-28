f8cadc22f0a055077f712d92d6dec9126a420770
vinicio <vinicio.borja10@gmail.com>
2025-02-27 19:57:46 -0500

    add abstract repository classes for Person, Role, RoleOption, RoleRoleOption, RoleUser, Session, and User

---
+++ src/domain/repositories/role-role-option.repository.ts
@@ -0,0 +1,20 @@
+import { CreateRoleRoleOptionDto, UpdateRoleRoleOptionDto } from "../dtos";
+import { RoleRoleOptionEntity } from "../entities/role_roleOption.entity";
+
+export abstract class RoleRoleOptionRepository {
+  abstract create(
+    createRoleRoleOptionDto: CreateRoleRoleOptionDto
+  ): Promise<RoleRoleOptionEntity>;
+  abstract getAll(): Promise<RoleRoleOptionEntity[]>;
+  abstract findById(
+    roleId: number,
+    roleOptionId: number
+  ): Promise<RoleRoleOptionEntity>;
+  abstract updateById(
+    updateRoleRoleOptionDto: UpdateRoleRoleOptionDto
+  ): Promise<RoleRoleOptionEntity>;
+  abstract deleteById(
+    roleId: number,
+    roleOptionId: number
+  ): Promise<RoleRoleOptionEntity>;
+}
---
+++ src/domain/repositories/role-user.repository.ts
@@ -0,0 +1,14 @@
+import { CreateRoleUserDto, UpdateRoleUserDto } from "../dtos";
+import { RoleUserEntity } from "../entities/role_user.entity";
+
+export abstract class RoleUserRepository {
+  abstract create(
+    createRoleUserDto: CreateRoleUserDto
+  ): Promise<RoleUserEntity>;
+  abstract getAll(): Promise<RoleUserEntity[]>;
+  abstract findById(roleId: number, userId: number): Promise<RoleUserEntity>;
+  abstract updateById(
+    updateRoleUserDto: UpdateRoleUserDto
+  ): Promise<RoleUserEntity>;
+  abstract deleteById(roleId: number, userId: number): Promise<RoleUserEntity>;
+}
